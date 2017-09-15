9a436b7bb454c28daf8a6cb772e5297faa259e14
nielstron <n.muendler@web.de>
2017-09-15 07:45:20 +0200

    Add the option to define a port, username and password in the config.yaml (#3)
    Added option to include username and password actually just set the [settings](https://www.npmjs.com/package/mqtt#connect) to the mqtt part of yaml, thus redirecting all set options in the yaml file to the mqtt library

--- server.js
+++ server.js
@@ -83,7 +83,8 @@ async.series([
     },
     function connectToMQTT (next) {
         winston.info('Connecting to MQTT at mqtt://%s', config.mqtt.host);
-        broker = mqtt.connect('mqtt://' + config.mqtt.host);
+	var mqtt_broker_options = config.mqtt;
+        broker = mqtt.connect(mqtt_broker_options);
         broker.on('connect', function () {
             next();
             // @TODO Not call this twice if we get disconnected
